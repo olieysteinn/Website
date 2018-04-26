9f01a2554b62b2fc7179e974a4b185f185b89a28
OliEysteinn <olafurey@gmail.com>
2018-04-26 09:41:53 +0000

    django 40

--- music/views.py
+++ music/views.py
@@ -40,3 +40,29 @@ class UserFormView(View):
     form_class = UserForm
     template_name = 'music/registration_form.html'
 
+    # display blank form
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+
+            # cleaned (normalized) data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+
+            # returns User objects if credentials are correct
+            user = authenticate(username=username, password=password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('music:index')
+
+        return render(request, self.template_name, {'form': form})
